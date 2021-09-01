58b607e324edd066e8ea18994078a4993c3c996b
Aravind Chowdary <kamaravichow@gmail.com>
2021-09-01 11:25:32 +0530

    initial project

---
+++ app/src/main/java/me/aravi/admobtemplates/MainActivity.java
@@ -0,0 +1,17 @@
+package me.aravi.admobtemplates;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.ads.nativetemplates.NativeTemplateStyle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
